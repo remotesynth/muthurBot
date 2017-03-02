2eb51b041f6648a1727f5d9ed75357ec0c729d60
Brian Rinaldi <brian.rinaldi@gmail.com>
2017-03-02 10:46:50 -0500

    tweak

--- plugins/selfdestruct.js
+++ plugins/selfdestruct.js
@@ -5,9 +5,9 @@ exports.setDestruct= function setDestruct(tm, cb) {
         pollInterval:60000
     });
     console.log(cb.userId);
-    //this.bot.directReply(1,"random","this is a message",function() {});
     timer.on('poll', function() {
         console.log("timer called");
+        //this.bot.reply(userId,messageString,callback);
     });
     timer.start();
     cb(null,'THE EMERGENCY DESTRUCT SYSTEM IS NOW ACTIVATED. THE SHIP WILL DETONATE IN T MINUS ' + tm + ' MINUTES.')
